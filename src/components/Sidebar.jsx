6ac0dbf180578305cd11ff5dea6612c0129907a3
Divyanshu Gautam <divyanshug7897@gmail.com>
2024-07-19 00:28:38 +0530

    Next-message

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,6 @@ import Navbar from "./Navbar"
 import Search from "./Search"
 import Chats from "./Chats"
 
-//lets begin Sidebar
 const Sidebar = () => {
   return (
     <div className="sidebar">
--- src/firebase.js
+++ src/firebase.js
@@ -2,22 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBDh3pbjj_QRZ_3hGDfzNslP2RyoIcSf0w",
-  authDomain: "createapp-ea67b.firebaseapp.com",
-  projectId: "createapp-ea67b",
-  storageBucket: "createapp-ea67b.appspot.com",
-  messagingSenderId: "569227794697",
-  appId: "1:569227794697:web:5c56d4726f643c13b51bd8",
-  measurementId: "G-SVKLK7B8MV"
-};  
-
+  apiKey: "AIzaSyAyHUA3BmehVXDaCtSMurQqMJ-hCG8iDqk",
+  authDomain: "streamline-83f91.firebaseapp.com",
+  projectId: "streamline-83f91",
+  storageBucket: "streamline-83f91.appspot.com",
+  messagingSenderId: "718978269839",
+  appId: "1:718978269839:web:c5310478cf7b7c49f226fa",
+  measurementId: "G-2X559GZJQT"
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const storage = getStorage();
-export const db = getFirestore(app)
+export const db = getFirestore()
